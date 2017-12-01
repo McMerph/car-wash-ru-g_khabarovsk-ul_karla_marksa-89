a17a05bebefee9affea3dfaad65ae9980a88484d
Aleksandr Sidorenko <mcmerph@gmail.com>
2017-12-01 16:50:59 +1000

    end of the day

--- src/main/app/view/TouchTest.ts
+++ src/main/app/view/TouchTest.ts
@@ -5,6 +5,7 @@ export default class TouchTest {
     private touch: Touch;
     private swipeStarted: boolean = false;
     private swipeDetecting: boolean = false;
+    private delta: number;
 
     public constructor() {
         this.element.addEventListener('touchstart', (event: TouchEvent) => this.handleTouchStart(event));
@@ -19,13 +20,13 @@ export default class TouchTest {
             this.touch = event.changedTouches[0];
 
             // TODO Delete
-            this.element.textContent =
-                `event: 'touchstart'
-                started: ${this.swipeStarted}
-                detecting: ${this.swipeDetecting}
-                this.touch.identifier: ${this.touch.identifier}
-                x: ${this.touch.pageX}
-                y: ${this.touch.pageY}`;
+            // this.element.textContent =
+            //     `event: 'touchstart'
+            //     started: ${this.swipeStarted}
+            //     detecting: ${this.swipeDetecting}
+            //     this.touch.identifier: ${this.touch.identifier}
+            //     x: ${this.touch.pageX}
+            //     y: ${this.touch.pageY}`;
         }
     }
 
@@ -44,7 +45,7 @@ export default class TouchTest {
                 }
                 if (this.swipeStarted) {
                     event.preventDefault();
-                    const delta: number = this.touch.pageX - touch.pageX;
+                    this.delta = this.touch.pageX - touch.pageX;
                     // Если листать некуда, делим смещение на некоторую величину
                     // для создания визуального эффекта «сопротивления движению» страницы.
                     // Таким образом, даем пользователю понять, что дальше страниц нет
@@ -52,26 +53,55 @@ export default class TouchTest {
                     //     delta = delta / 5;
                     // }
 
-                    this.element.textContent = `delta: ${delta}`;
-                    console.log(delta);
-                    // TODO Fix
-                    // this.moveTo(delta);
+                    this.moveTo(this.delta);
                 }
             }
         }
     }
 
     private handleTouchEnd(event: TouchEvent) {
-        if (event.touches.length === 1 && this.swipeStarted) {
+        console.log('JSON.stringify(event):', JSON.stringify(event));
+        if (this.swipeStarted) {
             event.preventDefault();
+            this.slideTo(this.delta);
 
             // Определяем, в какую сторону нужно произвести перелистывание
             // swipeTo = delta < 0 ? 'left' : 'right';
             // Отрисовываем перелистывание, о чем чуть позже
             // swipe(swipeTo);
-            this.element.textContent = `touchend`;
+            // this.element.textContent = `touchend`;
         }
         this.swipeStarted = false;
+    }
+
+    private slideTo(delta: number): void {
+        this.getLeftSlide().classList.add('slide-animating');
+        this.getCenterSlide().classList.add('slide-animating');
+        this.getRightSlide().classList.add('slide-animating');
+
+        if (delta < 0) {
+            this.slideToRight();
+        } else if (delta > 0) {
+            this.slideToLeft();
+        }
+    }
+
+    private slideToRight(): void {
+        this.getLeftSlide().style.transform = 'translate3d(0, 0, 0)';
+        this.getCenterSlide().style.transform = 'translate3d(100%, 0, 0)';
+    }
+
+    private slideToLeft(): void {
+        this.getRightSlide().style.transform = 'translate3d(0, 0, 0)';
+        this.getCenterSlide().style.transform = 'translate3d(-100%, 0, 0)';
+    }
+
+    private moveTo(delta: number): void {
+        const percentageDelta = delta / this.element.clientWidth * 100;
+
+        this.getLeftSlide().style.transform = `translate3d(${-100 - percentageDelta}%, 0, 0`;
+        this.getCenterSlide().style.transform = `translate3d(${-percentageDelta}%, 0, 0`;
+        this.getRightSlide().style.transform = `translate3d(${100 - percentageDelta}%, 0, 0`;
     }
 
     /**
@@ -82,6 +112,18 @@ export default class TouchTest {
      */
     private isHorizontalSwipe(touch: Touch) {
         return Math.abs(this.touch.pageX - touch.pageX) >= Math.abs(this.touch.pageY - touch.pageY);
+    }
+
+    private getLeftSlide() {
+        return (this.element.getElementsByClassName('slide-left')[0] as HTMLElement);
+    }
+
+    private getCenterSlide() {
+        return (this.element.getElementsByClassName('slide-center')[0] as HTMLElement);
+    }
+
+    private getRightSlide() {
+        return (this.element.getElementsByClassName('slide-right')[0] as HTMLElement);
     }
 
 }
