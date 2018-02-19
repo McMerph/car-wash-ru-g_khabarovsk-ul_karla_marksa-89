99d84f1b1dc645782f4f71da13fefe41c31debee
Aleksandr Sidorenko <mcmerph@gmail.com>
2018-02-19 16:01:32 +1000

    time-picker as design fix

--- src/main/app/view/components/date-time-picker/time-picker/TimePickerLayout.ts
+++ src/main/app/view/components/date-time-picker/time-picker/TimePickerLayout.ts
@@ -47,6 +47,7 @@ export default class TimePickerLayout extends ButtonsPickerLayout<Time> {
             grabCursor: true,
             mousewheel: true,
             slidesPerView: 5,
+            spaceBetween: 1,
         });
         this.buttons.forEach((button) => this.slider.appendSlide(SliderUtils.getSlide(button)));
         this.previousButton = SliderUtils.getPreviousButton(this.slider);
