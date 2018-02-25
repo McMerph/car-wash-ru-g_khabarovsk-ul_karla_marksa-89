e1dd60211bf725eb837b566228beac8da252191a
mcmerphy <mcmerph@gmail.com>
2018-02-25 19:10:24 +1000

    styles refactor

--- src/main/app/view/styles/index.ts
+++ src/main/app/view/styles/index.ts
@@ -1,6 +1,9 @@
 import "normalize.css/normalize.css";
 import "swiper/dist/css/swiper.min.css";
 
+import "./components.pcss";
+import "./layout.pcss";
+
 import "../components/footer/index.pcss";
 import "../components/header/index.pcss";
 import "../components/info/index.pcss";
