2542f7fd6ae9add1f9895ada59dc3ac48f58ef49
mcmerphy <mcmerph@gmail.com>
2018-02-25 16:34:42 +1000

    introduce styles folder

--- src/main/app/index.ts
+++ src/main/app/index.ts
@@ -2,15 +2,7 @@ import "eligrey-classlist-js-polyfill/classList.min.js";
 import * as padStart from "string.prototype.padstart";
 import "./StickyHoverFix.js";
 
-import "normalize.css/normalize.css";
-import "swiper/dist/css/swiper.min.css";
-import "./view/components/footer/index.pcss";
-import "./view/components/header/index.pcss";
-import "./view/components/info/index.pcss";
-import "./view/components/modal/index.pcss";
-import "./view/components/profile/index.pcss";
-import "./view/fonts.css";
-import "./view/index.pcss";
+import "./view/styles";
 
 import Modal from "./view/components/modal/index";
 
---
+++ src/main/app/view/styles/index.ts
@@ -0,0 +1,11 @@
+import "normalize.css/normalize.css";
+import "swiper/dist/css/swiper.min.css";
+
+import "../components/footer/index.pcss";
+import "../components/header/index.pcss";
+import "../components/info/index.pcss";
+import "../components/modal/index.pcss";
+import "../components/profile/index.pcss";
+
+import "./fonts.css";
+import "./index.pcss";
